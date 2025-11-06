17f89e9bc5656abc8e30cb1039e0d1b1519ad0fd
Andrees444 <159082675+ANDREE444@users.noreply.github.com>
2025-11-06 12:09:19 -0500

    Logo-CardioIA

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
+import logo from "../assets/CardioIA.png";
 
 function Header() {
   return (
     <header className="header">
       <nav className="nav-container">
         <div className="logo">
-          <Link to="/">CardioIA</Link>
+          <img src={logo} alt="CardioIA" className="logo-image" />
+          <Link to="/" className="logo-text">
+            CardioIA
+          </Link>
         </div>
         <ul className="nav-links">
           <li>
